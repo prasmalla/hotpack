19e29dca6cc10521fb487efbae74dd4857374876
prasmalla <malla.prasanna@gmail.com>
2019-12-17 19:07:45 -0500

    add boilerplate

---
+++ README.md
@@ -0,0 +1,21 @@
+Boilerplate for hot-reload webpack React application.
+Clone and run `yarn` to install and `yarn start` to start development.
+OR install packages and copy contents of [webpack.config.js](webpack.config.js) as required.
+
+```
+#initialize repo
+mkdir hotpack && cd hotpack
+git init
+yarn init -y
+
+#add eslint-airbnb-config with prettier
+exec 3<&1;bash <&3 <(curl https://gist.githubusercontent.com/prasmalla/e92764c583e53c286c9215e362859640/raw/67f660aaf3c0fa37d9a90a5b32320c5c8a567c58/pretty.sh 2> /dev/null)
+
+#add webpack/dev-server
+yarn global add webpack webpack-cli webpack-dev-server
+yarn add webpack webpack-cli webpack-dev-server html-webpack-plugin babel-loader @babel/core @babel/preset-env @babel/preset-react -D
+touch webpack.config.js
+
+#add entry/output - react for skeleton hello-hot-reload app
+yarn add react react-dom && mkdir src && mkdir dist && touch src/app.js && src/index.html && touch README.md
+```
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    inline: true,
+    open: true,
+    watchContentBase: true,
+    port: 8000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
